50402c65de18929720fdd86b4faa39d8a98a1e81
corssson <vanyakirill@bk.ru>
2025-09-24 20:14:39 +0500

    Добавьте файлы проекта.

---
+++ project1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string a = "Агния Барто";
+            Console.WriteLine(a);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string b = "ОСЕННЯЯ ПЕСЕНКА";
+            Console.WriteLine(b);
+            Console.ForegroundColor = ConsoleColor.Red;
+            string c = "Миновало лето, Осень наступила. \nНа полях и в рощах Пусто и уныло. \nПтички улетели, Стали дни короче, \nСолнышка не видно, Темны, темны ночи.";
+            Console.WriteLine(c);
+            Console.ResetColor();
+        }
+    }
+}
